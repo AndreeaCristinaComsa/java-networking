6f7da3ed660cea45e8cf7d12ae7d432e4aa5a946
Andreea <comsha.andreea@gmail.com>
2019-08-21 12:54:53 +0300

    Exercise 5 Using thread pooling for client-server applications Objectives     • Practice the usage of the thread pooling to develop a server that can be accessed by multiple clients. Description Change the following programs to use thread pooling for allowing multiple clients to access the server:     • EchoServer2 Use different methods to create a thread pool:     • Executors.newCachedThreadPool

--- src/com/luxoft/java_networking/example06/EchoServer2.java
+++ src/com/luxoft/java_networking/example06/EchoServer2.java
@@ -1,4 +1,6 @@
 package com.luxoft.java_networking.example06;
+import com.luxoft.java_networking.example07.ThreadPoolingServer;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,26 +8,45 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketAddress;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class EchoServer2 {
 	private int port;
+	private ExecutorService pool;
 
 	public EchoServer2(int port) {
 		this.port = port;
 	}
 
 	public void startServer() {
-		try (ServerSocket serverSocket = new ServerSocket(port)) {
-			System.out.println("EchoServer2 started on " + serverSocket.getLocalSocketAddress());
+//		try (ServerSocket serverSocket = new ServerSocket(port)) {
+//			System.out.println("EchoServer2 started on " + serverSocket.getLocalSocketAddress());
+//			while (true) {
+//				Socket socket = serverSocket.accept();
+//				Thread t = new Thread(new EchoThread(socket));
+//				t.start();
+//			}
+//		} catch (IOException e) {
+//			System.err.println(e);
+//		}
+//	}
+
+			try (ServerSocket serverSocket = new ServerSocket(port)) {
+			System.out.println("Server started on " + serverSocket.getLocalSocketAddress());
+			pool = Executors.newCachedThreadPool();
+		//pool = Executors.newSingleThreadExecutor();
+		//pool = Executors.newFixedThreadPool(10);
+
 			while (true) {
 				Socket socket = serverSocket.accept();
-				Thread t = new Thread(new EchoThread(socket));
-				t.start();
-			}
-		} catch (IOException e) {
-			System.err.println(e);
-		}
+				pool.execute(new EchoServer2.EchoThread(socket));
+						}
+	} catch (IOException e) {
+		System.err.println(e);
+		pool.shutdown();
 	}
+}
 
 	private class EchoThread implements Runnable {
 		private Socket socket;
